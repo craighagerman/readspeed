792be7bacd52a1b94d5d6dc17710466f90cd8830
Craig Hagerman <craighagerman@gmail.com>
2013-06-06 22:40:14 +0900

    Initial commit of readspeed Ruby on Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readspeed::Application.config.secret_token = '1daa109af509bffdb75862f4c9dce2e01f7b0c3e352f7ffcfc9fee175185c839da037f3ffe2a358619fff075f9626974d98134410304399eb88c6a7ceb04960f'
